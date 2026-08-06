a4379bd8f9f1c4ddb884ddc2ba1e66aebb18ce13
yuhobin <dptmzh12@gmail.com>
2026-08-06 12:47:55 +0900

    test commit

--- src/main/java/com/sist/web/SpringThymeLeafDockerProjectApplication.java
+++ src/main/java/com/sist/web/SpringThymeLeafDockerProjectApplication.java
@@ -2,7 +2,19 @@ package com.sist.web;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
+/*
+ *  git init ==> 연결하는 폴더 생성
+ *  git remote add origin 깃주소 => git 연결
+ *  git add . => 보낼 데이터 모으기
+ *  git commit -m "메세지"
+ *  git push -u origin master --force => 강제 전송
+ *  
+ *  -------------------------------------------
+ *  수정
+ *  git add .
+ *  git commit -m "메세지"
+ *  git push -u origin master
+ */
 @SpringBootApplication
 public class SpringThymeLeafDockerProjectApplication {
 
